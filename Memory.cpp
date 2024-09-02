2105fb2a8ad3e61ac0dcbf5f6d4f67537e56a509
KevinTrigg <144842371+KevinTrigg@users.noreply.github.com>
2024-09-02 16:19:06 -0400

    Update Memory.cpp wit hdebug statements

--- Memory.cpp
+++ Memory.cpp
@@ -16,18 +16,28 @@ char *mem;                      //array of chars used as memory
 
 
 Memory::Memory(){
-  this->mem = new char[256*blockSize]; // block size of 256, 256 blocks 
-  for(int i=0;i<sizeof(mem);i++){ //go through the full memory object
+  this->mem = new char[256*256]; // block size of 256, 256 blocks 
+  for(int i=0;i<(int)sizeof(mem);i++){ //go through the full memory object
     if(i<1){
-      mem[i]=0xF0; //reserve first 4 blocks for the bitmap and file headers
+      this->mem[i]=0xF0; //reserve first 4 blocks for the bitmap and file headers
     }else{
-      mem[i]=0x00;  //ensure they are created, but 0 
+      this->mem[i]=0x00;  //ensure they are created, but 0 
     }
   }
+  numberOfBlocks=256;   //how many blocks are in the memory
+  fileNameLength =32;   //how long a files name can be
+  maxFileCapacity=16;   //max number of files in the memory
+  bitMapOffset=32;      //number of byte offset to make space in mem for the bit map of the occupied block
+  fileHeaderLength=64; //size of each file header at start of mem behind the bitmap
+  blockSize=256;        //size of the blocks in bytes  
+  blocksInAFile=8;      //max number of blocks occupied by a file
+  blockIDLength=1;      //related to number of blocks, which specific block is indexed roundup(log(blockSize)/8)
+  fileTypeSize=1;       //size of the definition of the file type in the header
+  fileSizeLength=2;     //# of bytes to represent file size in bytes 2^11 =2048 extra bit used for spacing ease
 }
 
-Memory::Memory(char mem[]){ //if the user already has a memory allow them to copy it to this
-  this->mem=mem;
+Memory::Memory(char memIn[]){ //if the user already has a memory allow them to copy it to this
+  this->mem=memIn;
 }
 
 Memory::~Memory(){  //destructor of memory 
@@ -38,7 +48,7 @@ Memory::~Memory(){  //destructor of memory
 
 void Memory::createFile(char name[]){
 //check basic creation requirements
-  if (strlen(name)>=fileNameLength){
+  if ((int) strlen(name)>=fileNameLength){
     printf("name exceeds limits ensure name is less than %d\n",fileNameLength);
     return; //exit early, no allocation exit early, no allocation exit early, no allocation exit early, no allocation exit early, no allocation exit early, no allocation
   } else if(name[0] ==0x00){
@@ -63,6 +73,7 @@ void Memory::createFile(char name[]){
       } 
     } else {
       openFileSlot=true;
+      break;
     }
   }
   
@@ -70,13 +81,17 @@ void Memory::createFile(char name[]){
     printf("no open slot for a file, file cannot be created\n");
     return; //exit early, no allocation exit early, no allocation exit early, no allocation exit early, no allocation exit early, no allocation exit early, no allocation
   }
+  //int debugLocation;
   //if the file creation passes all errors, proceed to creating it
   for(int i=0;i<maxFileCapacity;i++){
     if(mem[(bitMapOffset + i*fileHeaderLength)]==0x00){ //check if this file name location is free
       for(int j=0;j<fileNameLength;j++){  //write the file name
+      //debugLocation= j+(bitMapOffset + i*fileHeaderLength);
+      if(j>=(int) strlen(name)){break;} //once the name is done, stop looping it also cuts the name at this length
         mem[j+(bitMapOffset + i*fileHeaderLength)] =name[j]; 
       }
       for(int j=0; j<fileHeaderLength-fileNameLength;j++){
+        //debugLocation=fileNameLength+(bitMapOffset + i*fileHeaderLength) +j ;
         mem[fileNameLength+(bitMapOffset + i*fileHeaderLength) +j ] =0x00;  //clear any junk data for the header infomation 
       }
       mem[(bitMapOffset + i*fileHeaderLength) + fileNameLength ] =0x00;  //set file type to .txt defaul 0x00
@@ -94,23 +109,24 @@ void Memory::write( char name[], char data[]){
   writeLocation[1]=0x00;//block offset
   writeLocation = findWriteLocation(name, writeLocation); //returns 2 bytes
   if(writeLocation[0] == 0x00 && writeLocation[1] == 0x00){ //null, values == errored out, don't write
-  printf("error writing\n");
+    printf("error writing\n");
+    delete writeLocation; 
     return; //breaks condition, it would overwrite the bitmap
   }
-
-  for(int write =0; write< strlen(data); write++){
+  int debugLocation;
+  for(int write =0; write< (int) strlen(data); write++){
+    debugLocation= writeLocation[0]*256+writeLocation[1];
     mem[writeLocation[0]*256+writeLocation[1]]=data[write]; //write the data
     writeLocation[1]= char((int(writeLocation[1])+1)%blockSize);  //increment the block size and if it's at the end of the block w
     if(writeLocation[1]==0x00){
       writeLocation = findWriteLocation(name, writeLocation);
       if(writeLocation[0] == 0x00 && writeLocation[1] == 0x00){ //null, values == errored out, don't write
+        delete writeLocation; 
           return; //breaks condition, it would overwrite the bitmap
       }
     }  
   }
-  //TODO: given write location, write to memory at that location then update file for occupied blocks, block offset, and file size 
-
-
+   delete writeLocation; 
   return;
 }
 
@@ -121,27 +137,33 @@ char* Memory::findWriteLocation(char name[],char returnable[] ){
   char tempBlockID= 0x00; //create a temp ID to find the last non 0 blockID in the file
   char blockOffset= 0x00; //what byte within the block to write to next
   // returnable: location is block position, offset in block
+  int debugLocation;
   for(int i=0;i<maxFileCapacity;i++){
-    if(mem[(bitMapOffset + i*fileHeaderLength)]==0x00){ //check if this file name location is occupied
+    debugLocation=(bitMapOffset + i*fileHeaderLength);
+    int debugChar= mem[(bitMapOffset + i*fileHeaderLength)];
+    if(mem[(bitMapOffset + i*fileHeaderLength)]!=0x00){ //check if this file name location is occupied
       for(int j=0;j<fileNameLength;j++){  //check the file name
+        debugLocation=j+(bitMapOffset + i*fileHeaderLength);
+        if(j>=(int) strlen(name)){break;} //once the name is done, stop looping it also cuts the name at this length
         if(mem[j+(bitMapOffset + i*fileHeaderLength)]!=name[j]){ // if the name doesn't match set match to false
           nameMatch=false;  
         } 
       }
 
       if(nameMatch){
+        debugLocation= bitMapOffset + (i*fileHeaderLength) + fileNameLength + fileTypeSize + blocksInAFile*blockIDLength +  fileSizeLength ;
         blockOffset= mem[bitMapOffset + (i*fileHeaderLength) + fileNameLength + fileTypeSize + blocksInAFile*blockIDLength +  fileSizeLength ]; 
         //load offset from  the file in question past the filename,type specifier,blocks,file size to get nextByte
-        for(int blockCheck= blocksInAFile-1; blockCheck<=0; blockCheck--){  //find the last block used by the file
+        for(int blockCheck= blocksInAFile-1; blockCheck>=0; blockCheck--){  //find the last block used by the file
           //check through the files block used space in reverse order
           //find the first non null value, if the nextByte is not equal to block size then use that block
           //if the nextByte IS equal to block size, get a new block
-                   
+            debugLocation=bitMapOffset + (i*fileHeaderLength) + fileNameLength +   fileTypeSize        +     blockCheck*blockIDLength;
             tempBlockID = mem[bitMapOffset + (i*fileHeaderLength) + fileNameLength +   fileTypeSize        +     blockCheck*blockIDLength]; 
             //          offset by map             name of file    how long the name is  what type of file byte    what block is loaded     byte of that blockID
             // load the next byte to the temp 
 
-          if(tempBlockID != 0x00){ //the first non null should be used
+          if(tempBlockID != 0x00 || blockCheck==0x00){ //the first non null should be used
             if(blockOffset==0x00){
               if(blockCheck==blocksInAFile-1){
                 printf("max size reached, can't write more\n");
@@ -150,13 +172,20 @@ char* Memory::findWriteLocation(char name[],char returnable[] ){
               }
               //if the block offset is 0 then a new block needs to be allocated (done on the start of new block not end of last block)
               tempBlockID = findEmptyBlock(); //
-              mem[bitMapOffset + (i*fileHeaderLength) + fileNameLength + fileTypeSize + (blockCheck+1)*blockIDLength]= tempBlockID;  
+              debugLocation= bitMapOffset + (i*fileHeaderLength) + fileNameLength + fileTypeSize + (blockCheck+1)*blockIDLength;
+              if(blockCheck==0){
+                mem[bitMapOffset + (i*fileHeaderLength) + fileNameLength + fileTypeSize + (blockCheck)*blockIDLength]= tempBlockID; 
+              }
+              else{
+                mem[bitMapOffset + (i*fileHeaderLength) + fileNameLength + fileTypeSize + (blockCheck+1)*blockIDLength]= tempBlockID;  
                                                                                   //write the NEXT block to be the new block
+              }
               }
             }
           }
           
         }
+        debugLocation=fileNameLength+(bitMapOffset + i*fileHeaderLength);
         mem[fileNameLength+(bitMapOffset + i*fileHeaderLength) ] =0x00;  //set file type to .txt defaul 0x00
         //all other blocks of file header are default null anyways so we don't need to adjust them
         break;  //if the space was open we only need to fill one slot, stop traversing the file name spaces
@@ -167,11 +196,13 @@ char* Memory::findWriteLocation(char name[],char returnable[] ){
   return returnable;
 }
 
-char findEmptyBlock(){
+char Memory::findEmptyBlock(){
   //need a new block
   char bitFromByte=0x00;
   char tempBlockID=0x00;
+  int debugLocation=0;
   for(int nextAvailableBlock= 0; nextAvailableBlock<bitMapOffset; nextAvailableBlock++ ){ //traverse the bitmap to find an available block
+  debugLocation=nextAvailableBlock;
     if(mem[nextAvailableBlock] < char(0xFF)){  //if the byte isn't FF that means there's a 0 (unused block) that can be used
       bitFromByte = mem[nextAvailableBlock];
       int bit=0;
@@ -183,7 +214,8 @@ char findEmptyBlock(){
           //set the temp block ID   
           mem[nextAvailableBlock] = mem[nextAvailableBlock] | (0x80>>bit);
           //occupy the block in bitmap
-          break;
+            return tempBlockID;
+        //  break;
         }
 
         bit++;
@@ -214,14 +246,17 @@ void Memory::dir(){
 void Memory::list(char fileName[]){
   //print out the contents of the file 
   //traverse through the blocks of the file (those that are occupied) and print out their contents
-  int whichFile = 0;  //find what file needs to be listed out based on the file name
+  //int whichFile = 0;  //find what file needs to be listed out based on the file name
   bool nameMatches=true; 
-  
+  int debugLocation=0;
   for(int i=0;i<maxFileCapacity;i++){ //go through each file in the file explorer
-      if(mem[(bitMapOffset + i*fileHeaderLength)]==0x00){ //check if this file name location is occupied
+      debugLocation=(bitMapOffset + i*fileHeaderLength);
+      if(mem[(bitMapOffset + i*fileHeaderLength)]!=0x00){ //check if this file name location is occupied
         nameMatches=true; //set the expectation that this it the file 
 
         for(int j=0;j<fileNameLength;j++){  //check the file name
+          debugLocation=j+(bitMapOffset + i*fileHeaderLength);
+          if(j>=(int) strlen(fileName)){break;} //once the name is done, stop looping it also cuts the name at this length
           if(mem[j+(bitMapOffset + i*fileHeaderLength)]!=fileName[j]){ // if the name doesn't match set match to false
             nameMatches=false;  
          } 
@@ -229,14 +264,16 @@ void Memory::list(char fileName[]){
         if (nameMatches){//if the file is successfully found
           // find the location of the first block from the file then go through it.
           for(int itterateThroughBlocks = 0; itterateThroughBlocks < blocksInAFile; itterateThroughBlocks++){
+            debugLocation=bitMapOffset + (i*fileHeaderLength) + fileNameLength + fileTypeSize + itterateThroughBlocks*blockIDLength;
             char blockLocation = mem[bitMapOffset + (i*fileHeaderLength) + fileNameLength + fileTypeSize + itterateThroughBlocks*blockIDLength];
             //once we have the location we need to test if it's valid then read memory from that block
             if (blockLocation==0x00){
-              printf("reached the end of the file\n");
+              printf("\n\nreached the end of the file\n");
               break;  //the block is null so we don't need to try the rest of the blocks
             }
             for(int blockByte=0; blockByte < blockSize; blockByte++){ //print out the contents of the block byte by byte
-              printf("%c",mem[int(blockLocation) +blockByte]);
+              debugLocation=blockLocation*blockSize+blockByte;
+              printf("%c",mem[blockLocation*blockSize+blockByte]);
             }
           }
         }

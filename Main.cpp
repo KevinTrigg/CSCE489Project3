6c1e0c3794e4f2f6174c2586345b1621b7df4d98
KevinTrigg <144842371+KevinTrigg@users.noreply.github.com>
2024-09-02 16:18:43 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -9,14 +9,22 @@ int main(){
 Memory *mem = new Memory();
 
 
-char myword[] = "Hello";
+
+char fileName1[] = "Mike Smith";
+char* pname1 = &fileName1[0];
+char fileContent1[] = "Dragic";
+char* content1F1 = &fileContent1[0];
 char str[] = "Mike Smith";
-char str2[] = "Dragic";
-
 char* pname = &str[0];
-//char* pteam = &str2[0];
-
+char str2[] = "Dragic";
+char* pteam = &str2[0];
 
 mem->createFile(pname);
-
+mem->dir();
+mem->write(pname,pteam);
+mem->list(pname);
+mem->createFile(pname);
+mem->dir();
+mem->write(pname,pteam);
+mem->list(pname);
 }
